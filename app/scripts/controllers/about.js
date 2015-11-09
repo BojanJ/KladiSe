a2d86484a29d05cddca8c9396bd298d3d52aa7ac
Bojan Janevski <bojan.janevski@interway.com.mk>
2015-11-09 15:39:44 +0100

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kladiSeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kladiSeApp
+ */
+angular.module('kladiSeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kladiSeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kladiSeApp
+ */
+angular.module('kladiSeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
